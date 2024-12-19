7442a685d260ee984bf8fc1dde5854a699479728
Gautam Thulasiraman <111104742+gautamkohli18@users.noreply.github.com>
2024-12-19 18:07:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ generation_config = {
     "temperature": 1,
     "top_p": 0.95,
     "top_k": 40,
-    "max_output_tokens": 8192,
+    "max_output_tokens": 118192,
     "response_mime_type": "text/plain",
 }
 
